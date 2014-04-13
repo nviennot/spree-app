8d6341c28ea370975ba7198b12b321ef00319fcb
Nicolas Viennot <nicolas@viennot.biz>
2014-04-13 04:42:46 -0400

    updates

--- config/initializers/promiscuous.rb
+++ config/initializers/promiscuous.rb
@@ -1,4 +1,7 @@
 Promiscuous.configure do |config|
-  config.logger = Logger.new('log/promiscuous.log')
+  config.publisher_exchange   = 'spree-app'
+  config.subscriber_exchanges = ['diaspora', 'diaspora-topic']
+
+  config.logger = Logger.new(STDERR)
   config.logger.level = 0
 end
